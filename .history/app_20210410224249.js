b7f4e4b5f744faac795df2a50a426b3e1b240362
stephon-xuan <1405028323@qq.com>
2021-04-11 00:40:53 +0800

    feat: 接口初步完成

---
+++ .history/app_20210410224249.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressJwt = require("express-jwt")
+var compression = require('compression');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var roomRouter = require('./routes/livingRoom');
+
+var app = express();
+
+//开启gzip
+app.use(compression());
+
+//设置跨域头
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization,Origin,Accept,X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('X-Powered-By', ' 3.2.1');
+  res.header('Content-Type', 'application/json;charset=utf-8');
+  next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//使用中间件
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressJwt({
+  secret:"living_xiaoyu",
+  algorithms:['HS256'],
+  credentialsRequired:true, //是否校验
+}).unless({
+  path:['/users/login','/livingRoom/roomList','/livingRoom/roomListByType']
+}))
+
+//路由相关
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/livingRoom", roomRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
